b9ded80946249a714ea4074c353667b90406d972
vinitraj10 <vinitraj1012@gmail.com>
2019-01-10 21:11:06 +0530

    Assignment logic donegit add A1-RESTAPI/

--- A1-RESTAPI/index.js
+++ A1-RESTAPI/index.js
@@ -1,110 +1,135 @@
-/*
-* Primary File for the API
-*
-*/
-
 // Dependencies
-var http = require('http');
+
 var https = require('https');
+var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./config');
-
-// Intantiate the http server
-var httpServer = http.createServer(function(req,res){
-  unifiedServer(req,res);
-});
-
-// Start listening on port 3000
-httpServer.listen(config.httpPort,function(){
-  console.log("Server running on port "+config.httpPort);
-});
-
-var httpsServerOptions = {
-  'key':fs.readFileSync('./https/key.pem'),
-  'cert':fs.readFileSync('./https/cert.pem')
-}
-// Intantiate the https server
-var httpsServer = https.createServer(httpsServerOptions,function(req,res){
-  unifiedServer(req,res);
-});
-
-httpsServer.listen(config.httpsPort,function(){
-  console.log("Server running on port "+config.httpsPort);
-});
 
 
-var unifiedServer = function(req,res){
-  // Get the url and parse it
-  var parsedUrl = url.parse(req.url,true);
-
-  // Get the path
-  var path = parsedUrl.pathname;
-  // Deletes the unnecessary '/'
-  var trimmedPath = path.replace(/^\/+|\/+$/g,'');
-
-  // Get the queryStrings
-  var queryStringObjects = parsedUrl.query;
-  // Get the HTTP method
-  var method = req.method.toUpperCase();
-
-  // Get request headers
-  var headers = req.headers;
-
-  var decoder = new StringDecoder('utf-8');
-  var buffer = '';
-  req.on('data',function(data){
-    buffer+=decoder.write(data);
-  });
-  req.on('end',function(){
-    buffer+=decoder.end();
-
-    // checking whether the url entered exists or not in our routes if not it will go to not found page.
-    var choosenHandler = typeof(routes[trimmedPath]) !== 'undefined' ? routes[trimmedPath] : handlers.notFound;
-    
-    // create data object that is to be send to the handler
-
-    var data = {
-      'trimmedPath': trimmedPath,
-      'queryStringObjects': queryStringObjects,
-      'method': method,
-      'headers': headers,
-      'payload': buffer
-    };
-    
-    // calling this function will route our request by calling the function which is assinged for this url
-    choosenHandler(data,function(statusCode,payload){
-      // Use status code from the handler or set default to 200
-      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-      // Use payload from the handler or set default to {}
-      payload = typeof(payload) == 'object' ? payload : {};
-
-      // Can't return object as response it must be type of string or buffer
-      payloadString = JSON.stringify(payload)
-      // Send the response
-      res.writeHead(statusCode);
-      res.end(payloadString);
-
-      // log the request path
-      console.log('Returning the response',payload,statusCode);
-    });
-  });
-}
-// Defining handlers
+// It is an object which contains functions for the required url path.
 var handlers = {};
 
-// hello handler
+// handler for url /hello/
 handlers.hello = function(data,callback){
-  callback(200,{'message':'Successfully done'})
-}
-// notFound handler
-handlers.notFound = function(data,callback){
-  callback(404);
+	/*
+		All the business logic for any particular url should be written here.
+		like if it was a registration url handler the database communication 
+		would have taken place here.
+	*/
+	if(data.method != 'POST') {
+		callback(405,{'message':'Method Not allowed'});
+	}
+	else {
+		callback(200,{'message':'Welcome User!!'});
+	}
 }
 
-// Defining routes
+// handler for not found urls
+handlers.notfound = function(data,callback){
+	callback(404);
+}
 
+// URL path which will call the logic for that url
 var routes = {
-  'hello': handlers.hello
+	'hello':handlers.hello
 }
+
+// https server options
+var httpsServerOptions = {
+	'key':fs.readFileSync('./https/key.pem'),
+	'cert':fs.readFileSync('./https/cert.pem')
+}
+
+// Instantiate HTTPS server
+var httpsServer = https.createServer(httpsServerOptions,function(req,res){
+	unifiedServer(req,res);
+});
+
+// Instantiate HTTP server
+var httpServer = http.createServer(function(req,res){
+	unifiedServer(req,res);
+});
+
+// UnifiedServer which is passed to both http and https protocol since all the functionalities is same
+
+var unifiedServer = function(req,res){
+	/* Get the reqest url and parse it second parameter 
+	   is true for query params it returns a url object 
+	   which has information for the url
+	*/
+	var parsedUrl = url.parse(req.url,true);
+
+	// Get the url path like - /hello/
+	var path = parsedUrl.pathname;
+
+	// Deletes unnecessary slashes
+	var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+	/*
+		To get query object from url we can do the following -
+		var queryStringObject = path.query;
+		To get request headers we can do the following -
+		var headers = req.headers;
+	*/
+
+	// Get the http request method
+	var method = req.method.toUpperCase();
+
+	// Stringdecoder for getting the data from the http request
+	var decoder = new StringDecoder('utf-8');
+
+	// Empty string to hold the data coming from the request
+	var buffer = '';
+
+	// Append data to buffer as data is coming from the request on data event.
+	req.on('data',function(data){
+		buffer+=decoder.write(data);
+	});
+
+	// Handling the data after it gets end on end event
+	req.on('end',function(){
+		buffer+=decoder.end();
+
+		// Finding which handler to call after getting the url path
+		var choosenHandler = typeof(routes[trimmedPath]) !== 'undefined' ? routes[trimmedPath] : handlers.notfound;
+		
+		// Creating data object which is to be send to the handler for manipulation if required 
+		var data = {
+			'trimmedPath':trimmedPath,
+			//'queryStringObject':queryStringObject,
+			'method':method,
+			//'headers':headers
+			'payload':buffer
+
+		};
+
+		// call the handler for the url entered
+		choosenHandler(data,function(statusCode,payload){
+			// Get the status code and if not provided the set it 200 as default
+			statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+			// Get the response payload from the handler that is to be send to the user
+			payload = typeof(payload) == 'object' ? payload : {};
+
+			// Stringify the payload because response can not be a object
+			payloadString = JSON.stringify(payload);
+			// Sending the response with status code  
+			res.writeHead(statusCode);
+			res.end(payloadString);
+
+			// Logging request in the terminal
+			console.log('Returning the response',payload,statusCode);
+		});
+
+	});
+}
+
+// HTTP server listening on 8080 port
+httpServer.listen(8080,function(){
+	console.log("HTTP Server running at 8080");
+});
+
+// HTTPS server listening on 8081 port
+httpsServer.listen(8081,function(){
+	console.log("HTTPS Server running at 8081");
+});
